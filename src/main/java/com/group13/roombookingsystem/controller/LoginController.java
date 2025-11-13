0819ea28c9bd81f0ba0d4cb0c29a874e3f1cf376
Sajad <wbayatsb@gmail.com>
2025-11-13 16:40:01 -0500

    added signup view

--- src/main/java/com/group13/roombookingsystem/controller/LoginController.java
+++ src/main/java/com/group13/roombookingsystem/controller/LoginController.java
@@ -1,5 +1,36 @@
 package com.group13.roombookingsystem.controller;
 
+import com.group13.roombookingsystem.view.LoginView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
 
+    public Button loginButton;
+    public Label errorLabel;
+    public TextField emailTextField;
+    public PasswordField passwordTextField;
+
+    public void hangleLogin(ActionEvent actionEvent) {
+        System.out.println("email: " + emailTextField.getText() + ", password: " + passwordTextField.getText());
+        emailTextField.setText("");
+        passwordTextField.setText("");
+    }
+
+
+    public void showSignUpView(MouseEvent mouseEvent) throws IOException {
+        Stage stage = ((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginView.class.getResource("/fxml/SignUp.fxml"));
+        stage.setScene(new Scene(fxmlLoader.load()));
+    }
 }
---
+++ src/main/java/com/group13/roombookingsystem/controller/SignUpController.java
@@ -0,0 +1,42 @@
+package com.group13.roombookingsystem.controller;
+
+import com.group13.roombookingsystem.view.LoginView;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SignUpController implements Initializable{
+    public TextField nameTextField;
+    public Button SignUpButton;
+    public Label signUpLabel;
+    public Label errorLabel;
+    public PasswordField confirmPasswordTextField;
+    public TextField emailTextField;
+    public PasswordField passwordTextField;
+    public ComboBox<String> accountTypeComboBox;
+    private final String[] accountTypes = {"Student", "Faculty", "Staff", "Partner"};
+
+    public void hangleSignUp(ActionEvent actionEvent) {
+        errorLabel.setText(accountTypeComboBox.getValue());
+    }
+
+    public void showLoginView(MouseEvent mouseEvent) throws IOException {
+        Stage stage = ((Stage) ((Node) mouseEvent.getSource()).getScene().getWindow());
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginView.class.getResource("/fxml/Login.fxml"));
+        stage.setScene(new Scene(fxmlLoader.load()));
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        accountTypeComboBox.getItems().addAll(accountTypes);
+    }
+}
