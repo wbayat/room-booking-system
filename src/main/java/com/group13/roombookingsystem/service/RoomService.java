6ec47f350cf46488d72c9e3453d4c8549412207b
Sajad <wbayatsb@gmail.com>
2025-11-13 19:41:51 -0500

    fixed the structure

---
+++ src/main/java/com/group13/roombookingsystem/service/RoomService.java
@@ -0,0 +1,8 @@
+package com.group13.roombookingsystem.service;
+import java.util.ArrayList;
+
+import com.group13.roombookingsystem.model.room.Room;
+
+public class RoomService {
+    public ArrayList<Room> users = new ArrayList<>();
+}
---
+++ src/main/java/com/group13/roombookingsystem/service/UserService.java
@@ -0,0 +1,8 @@
+package com.group13.roombookingsystem.service;
+import java.util.ArrayList;
+
+import com.group13.roombookingsystem.model.user.User;
+
+public class UserService {
+    public ArrayList<User> users = new ArrayList<>();
+}
