67d5bf717d857c4ad2297fbcd9fd87eb5a6b7854
Sajad <s.b.bayat78@gmail.com>
2025-11-21 14:47:15 -0500

    added ModifyBookingView and Controllers

--- src/main/java/com/group13/roombookingsystem/controller/user/ModifyBookingController.java
+++ src/main/java/com/group13/roombookingsystem/controller/user/ModifyBookingController.java
@@ -1,4 +1,31 @@
 package com.group13.roombookingsystem.controller.user;
 
-public class ModifyBookingController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ModifyBookingController implements Initializable {
+    public Label roomLocation1;
+    public Label roomID;
+    public Label roomLocation;
+    public Label roomCapacity;
+    public DatePicker checkInDate;
+    public ComboBox<String> checkinTime;
+    public ComboBox<String> checkoutTime;
+
+    private final String[] times = {"8 AM","10 AM","12 PM","2 PM","4 PM","6 PM","8 PM"};
+
+    public void handleConfirmModify(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        checkinTime.getItems().addAll(times);
+        checkoutTime.getItems().addAll(times);
+    }
 }
