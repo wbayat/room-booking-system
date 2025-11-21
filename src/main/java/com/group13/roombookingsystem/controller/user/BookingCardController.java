a2c5821086b2b7b6a663ec3ed141281a0d7248d7
Sajad <s.b.bayat78@gmail.com>
2025-11-21 11:57:42 -0500

    added projector and speaker checks to addRoom card

--- src/main/java/com/group13/roombookingsystem/controller/user/BookingCardController.java
+++ src/main/java/com/group13/roombookingsystem/controller/user/BookingCardController.java
@@ -1,27 +1,41 @@
 package com.group13.roombookingsystem.controller.user;
 
+import com.group13.roombookingsystem.model.booking.Booking;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.io.IOException;
 
 public class BookingCardController {
-    public Label RoomID;
+    private Booking booking;
     public Label checkinDate;
     public Label checkinTime;
     public Label checkoutTime;
+    public Label RoomName;
+    public Label RoomLocation;
+    public Button modifyButton;
+    public Button cancelButton;
 
     public void setData(){
-        RoomID.setText("Room A - BRG");
+        RoomName.setText("Room A - BRG");
         checkinDate.setText("November 20, 2025");
         checkinTime.setText("3:00 PM");
         checkoutTime.setText("8:00 PM");
     }
+    
 
-    public void handleExtend(ActionEvent mouseEvent) throws IOException {
+    public void handleCancelBooking(ActionEvent actionEvent) {
     }
 
-    public void handleCancel(ActionEvent mouseEvent) throws IOException {
+    public void handleModifyBooking(ActionEvent actionEvent) {
     }
 
+    public Booking getBooking() {
+        return booking;
+    }
+
+    public void setBooking(Booking booking) {
+        this.booking = booking;
+    }
 }
