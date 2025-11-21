f91a8481e50f091761468eca822a8a9a58859abb
Sajad <s.b.bayat78@gmail.com>
2025-11-21 09:27:39 -0500

    fixed imports in ViewFactory

--- src/main/java/com/group13/roombookingsystem/view/ViewFactory.java
+++ src/main/java/com/group13/roombookingsystem/view/ViewFactory.java
@@ -1,8 +1,8 @@
 package com.group13.roombookingsystem.view;
 
 import com.group13.roombookingsystem.model.user.User;
-import com.group13.roombookingsystem.model.user.university.admin.Admin;
-import com.group13.roombookingsystem.model.user.university.admin.chief.ChiefEventCoordinator;
+import com.group13.roombookingsystem.model.user.Admin;
+import com.group13.roombookingsystem.model.user.ChiefEventCoordinator;
 
 import java.io.IOException;
 
